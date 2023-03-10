ce4bc3bdb21ea009950e284712dd96b4953afa6b
TheoOrlando <112376347+TheoOrlando@users.noreply.github.com>
2023-03-10 15:43:10 +0100

    Update home page

--- PhP/view/home.php
+++ PhP/view/home.php
@@ -3,7 +3,7 @@ $title = 'Accueil';
 ob_start();
 ?>
 <div class="card" style="width: 75rem;">
-    <div class="card-header justify-content-center flex">
+    <div class="card-header justify-content-center d-flex">
     Liste des matches du jour
     </div>
     <ul class="list-group list-group-flush">
@@ -11,7 +11,7 @@ ob_start();
             foreach ($games as $game)
             {
                 ?>
-                <a href='?action=game&id=".$game->id."' class="link"><ul class="list-inline d-flex justify-content-around"><li class="list-inline-item"><?=$game->receivingTeamName?></li><li class="list-inline-item"><?=$game->moment?></li><li><?=$game->visitingTeamName?></li></ul></a>
+                <a href='?action=game&id=".$game->id."' class="link"><ul class="list-inline d-flex justify-content-around"><li class="list-inline-item"><?=$game->receivingTeamName?></li><li><?=$game->visitingTeamName?></li><li class="list-inline-item"><?=$game->moment?></li></ul></a>
                 <?php
             }
         ?>
