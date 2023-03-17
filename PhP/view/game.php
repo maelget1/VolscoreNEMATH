1a6d24769e94855ac86b61a158bac01ff5982176
NeoMasson <112376310+NeoMasson@users.noreply.github.com>
2023-03-17 15:23:13 +0100

    Update game.php
    ajout des capitaines et liberos

--- PhP/view/game.php
+++ PhP/view/game.php
@@ -7,13 +7,23 @@ $title = $actualGame->receivingTeamName.'-'.$actualGame->visitingTeamName;
 <br><br>
 
 <h1>Résultats du match</h1>
-<div >
-    <div><?=$actualGame->moment?></div>
-</div>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-4 text-start p-3">
+        <p><?=$actualGame->moment?></p>
+      </div>
+      <div class="col-4 text-center p-3">
+        <p><?=$actualGame->place?></p>
+      </div>
+      <div class="col-4 text-end p-3">
+        <p>Aligné à droite</p>
+      </div>
+    </div>
+  </div>
 <table class="table table-bordered border-dark">
   <tr><th><?=$actualGame->receivingTeamName?></th><th><?=$actualGame->scoreReceiving?></th><th class="mx-auto"><?=$actualGame->league?> <?=$actualGame->type?></th><th><?=$actualGame->scoreVisiting?></th><th><?=$actualGame->visitingTeamName?></th></tr>
-  <tr><td class="mr-5">Capitaine: N°32<?php ?></td></tr>
-  <tr><td class="mr-5">Libero(s): <?=$liberoTeam1?></td></tr>
+  <tr><td class="mr-5">Capitaine: N°<?=$captainTeam1->number?></td>Capitaine: N°<?=$captainTeam2->number?></tr>
+  <tr><td class="mr-5">Libero(s): N°<?=$liberoTeam1->number?></td><td>Libero(s): N°<?=$liberoTeam2->number?></td></tr>
   <tr><td class="mr-5">Carton(s): N°3  <?php ?><img src="..\images\cartonRouge.png" alt="carton rouge"></td></tr>
 </table>
 
