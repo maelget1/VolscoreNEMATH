04e88e66f02c4e0bfd8183f14f3c02af22186b69
maelget1 <112376326+maelget1@users.noreply.github.com>
2023-03-17 13:46:17 +0100

    enlever la partie d'obtention d'id qui ne fonctionne pas bien

--- PhP/view/playedGames.php
+++ PhP/view/playedGames.php
@@ -3,7 +3,7 @@ $title = 'Matches joués';
 ob_start();
 ?>
 <div class="card" style="width: 75rem;">
-    <div class="card-header justify-content-center flex">
+    <div class="card-header justify-content-center d-flex">
     Liste des matches joués
     </div>
     <ul class="list-group list-group-flush">
@@ -11,7 +11,7 @@ ob_start();
             foreach ($games as $game)
             {
                 ?>
-                <a href='?action=game&id=".$game->id."' class="link"><ul class="list-inline d-flex justify-content-around"><li class="list-inline-item"><?=$game->receivingTeamName?>-<?=$game->scoreReceiving?></li><li class="list-inline-item"><?=$game->moment?></li><li class="list-inline-item"><?=$game->scoreVisiting?>-<?=$game->visitingTeamName?></li></ul></a>
+                    <a href="?action=game&id=" class="link"><ul class="list-inline d-flex justify-content-around"><li class="list-inline-item"><?=$game->receivingTeamName?>-<?=$game->scoreReceiving?></li><li class="list-inline-item"><?=$game->moment?></li><li class="list-inline-item"><?=$game->scoreVisiting?>-<?=$game->visitingTeamName?></li></ul></a>
                 <?php
             }
         ?>
