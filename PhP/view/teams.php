1faa0a9a87bb2877730adb0676ced6cc8a902d0c
NeoMasson <112376310+NeoMasson@users.noreply.github.com>
2023-05-26 14:36:14 +0200

    améliorer le design de la page teams

--- PhP/view/teams.php
+++ PhP/view/teams.php
@@ -4,18 +4,68 @@ session_start();
 ob_start();
 ?>
 
-<h1>Equipes</h1>
-<ul>
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?=$title?></title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      padding: 20px;
+    }
 
-<?php
-foreach ($teams as $team)
-{
-    ?>
-    <li><?=$team->name?><a href="https://www.google.com"><img src="..\images\loupe.png" class="img-fluid" alt="Details"></a></li>
-    <?php
-}
-?>
-</ul>
+    h1 {
+      color: #333333;
+      text-align: center;
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    li {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px;
+      background-color: #ffffff;
+      border-radius: 5px;
+      margin-bottom: 10px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    li img {
+      width: 20px;
+      height: 20px;
+    }
+
+    a {
+      color: #333333;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <h1>Equipes</h1>
+  <ul>
+    <?php foreach ($teams as $team) { ?>
+      <li>
+        <?=$team->name?>
+        <a href="https://www.google.com">
+          <img src="..\images\loupe.png" class="img-fluid" alt="Details">
+        </a>
+      </li>
+    <?php } ?>
+  </ul>
+</body>
+</html>
 
 <?php
 $content = ob_get_clean();
