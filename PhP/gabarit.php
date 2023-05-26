86626c65230072b3433b1c736456ca4e0565329f
maelget1 <112376326+maelget1@users.noreply.github.com>
2023-05-26 15:39:53 +0200

    sauvetage miraculeux

--- PhP/gabarit.php
+++ PhP/gabarit.php
@@ -41,7 +41,7 @@
 <?= $content ?>
 </div>
 <footer class="text-center mt-auto">
-    <p>© ETML 2023</p>
+    <p>© Néo Masson, Theo Orlando, Maël Gétain   ETML 2023 V1.0</p>
 </footer>
 </body>
 </html>
